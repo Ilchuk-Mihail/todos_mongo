bff2a2f757771885533987a3356dd8453c2c8bd6
Vasyl <kushnir.vasyl.lv@gmail.com>
2021-09-16 18:00:09 +0300

    remove comments

--- src/controller/item.ts
+++ src/controller/item.ts
@@ -4,16 +4,14 @@ import TaskModel, { Task } from '../model/task'
 export default {
   async createItem (req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-    const { title, description, importance } = req.body
-    const newTask: Task = await TaskModel.create({
-      title,
-      description
-    })
+      const { title, description } = req.body
+      const newTask: Task = await TaskModel.create({
+        title,
+        description
+      })
       res.status(201).json(newTask)
-        } catch(err) {
-            next(err)//create middleware - handle error functions)
-        }
+    } catch (err) {
+      next(err)
     }
+  }
 }
-
-
