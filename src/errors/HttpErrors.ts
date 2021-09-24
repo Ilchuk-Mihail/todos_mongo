a9510b70b22e8b6f8e2bd9a4e360541665756634
Vasyl <kushnir.vasyl.lv@gmail.com>
2021-09-24 10:15:30 +0300

    fix http errors

--- src/errors/HttpErrors.ts
+++ src/errors/HttpErrors.ts
@@ -1,17 +1,13 @@
 import BaseError from './BaseError'
 
 export class TaskNotFoundError extends BaseError {
-  constructor (status = 404, message = 'Task not found') {
-    super(status, message)
-    this.status = status
-    this.message = message
+  constructor () {
+    super(404, 'Task not found')
   }
 }
 
 export class InvalidIdError extends BaseError {
-  constructor (status = 400, message = 'Invalid Id') {
-    super(status, message)
-    this.status = status
-    this.message = message
+  constructor () {
+    super(400, 'Invalid Id')
   }
 }
