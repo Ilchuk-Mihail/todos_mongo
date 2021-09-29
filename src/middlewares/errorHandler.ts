44d1b33af9d4325aede6a819a42726f1b2962b16
Vasyl <kushnir.vasyl.lv@gmail.com>
2021-09-29 14:18:08 +0300

    add refactoring due to add no-unused-vars eslint rule

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,8 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
 import BaseError from '../errors/BaseError'
 import logger from '../lib/logger'
 
-export default function errorHandler (error: BaseError, req: Request, res: Response, next: NextFunction): void {
+export default function errorHandler (error: BaseError, req: Request, res: Response): void {
   const status = error.status || 500
   const message = error.message || 'Something went wrong '
   const meta = error.meta || {}
