ca65d4471ffe5d18ad213f65f848e0216ab78e1b
Vasyl <kushnir.vasyl.lv@gmail.com>
2021-10-11 11:19:23 +0300

    refactoring, add auth to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express'
 import taskRouter from './routes'
-import { connection } from './lib/db'
+import db from './lib/db'
 import config from './config'
 import errorHandler from './middlewares/errorHandler'
 import logger from './lib/logger'
@@ -8,7 +8,7 @@ import logger from './lib/logger'
 const app: Application = express()
 const port = config.get('PORT')
 
-connection().catch((err: any) => err)
+db.connection()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
 import config from '../config/index'
 
-const uri = config.get('MONGO_URI')
-// const testUri = config.get('MONGO_URI_TEST')
-export async function connection (): Promise<void> {
-  return mongoose.connect(uri)
-    .then(_result => mongoose.connection)
-    .catch(err => err.logger)
+const user = config.get('MONGODB_USER')
+const password = config.get('MONGODB_PASSWORD')
+const connection = config.get('MONGODB_CONNECTION')
+const db = config.get('MONGODB_DATABASE')
+const uri = `mongodb://${user}:${password}@${connection}:27017/${db}`
+const uriMongoDocker = 'mongodb://admin:admin@mongo:27017/?authSource=admin'
+
+export default {
+  async connection (): Promise<void> {
+    mongoose.connect(uri)
+      .catch(err => err.logger)
+  }
 }
